d8bf8efb8b7a300bf95b53f1812721c751bb6c76
Cyril <cyril@nexu.org>
2017-10-16 20:21:08 +0200

    Router Function a lot of errors

--- src/main/java/ch/collen/reactive/ReactiveDemo5/ReactiveDemo5Application.java
+++ src/main/java/ch/collen/reactive/ReactiveDemo5/ReactiveDemo5Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -25,6 +28,13 @@ public class ReactiveDemo5Application {
                                         .map(s -> new Movie(UUID.randomUUID().toString(), s))
                                         .collect(Collectors.toList())
                         ).subscribe(System.out::println));
+    }
+
+    @Bean
+    public RouterFunction<?> routes(FluxMovieService service) {
+        return RouterFunctions.route(
+                RequestPredicates.GET("/movies"), req -> req.bodyToFlux(Movie.class)) //FIXME to finish
+        )
     }
 
 }
